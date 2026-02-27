922a946101d740edc187917462f3034f413c3195
Shaban Irshad <shabanirshad408@gmail.com>
2026-02-27 21:14:07 +0530

    done

---
+++ src/components/ButtonCounter.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const ButtonCounter=()=>{
+    const [count,setCount]=useState(0);
+    return(
+        <>
+            <p>Button clicked {count} times</p>
+            <button onClick={()=>setCount(count+1)}>Click me</button>
+        </>
+    );
+}
+export default ButtonCounter;
